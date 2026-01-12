f7cada7cf6dacda3ebfedee757adb67fa6c435ad
LuizBuarqueDev <luizgabrielbuarque@gmail.com>
2026-01-12 20:26:31 -0300

    Initialize Spring Boot project with MySQL support and project structure

---
+++ prateleira-inteligente-v2/src/main/java/br/com/lgbv/prateleira_inteligente_v2/PrateleiraInteligenteV2Application.java
@@ -0,0 +1,12 @@
+package br.com.lgbv.prateleira_inteligente_v2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrateleiraInteligenteV2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrateleiraInteligenteV2Application.class, args);
+	}
+}
